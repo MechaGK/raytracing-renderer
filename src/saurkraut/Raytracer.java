473d6db63b2b2bdb88336c70f79519bb6b1b7af2
Mads Grau Kristensen <madsgrau@gmail.com>
2017-05-04 14:57:01 +0200

    Split renderScene into multiple methods

--- src/saurkraut/Raytracer.java
+++ src/saurkraut/Raytracer.java
@@ -6,9 +6,39 @@ import saurkraut.util.ColorUtil;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Iterator;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.List;
 
 public class Raytracer {
+    private static class ImageRayHit {
+        public final int imageX;
+        public final int imageY;
+        public final Ray ray;
+        public final Vector3D point;
+        public final Shape shape;
+
+        private ImageRayHit(int imageX, int imageY, Ray ray, Vector3D point, Shape shape) {
+            this.imageX = imageX;
+            this.imageY = imageY;
+            this.ray = ray;
+            this.point = point;
+            this.shape = shape;
+        }
+    }
+
+    private static class ShadingPoint {
+        public final int imageX;
+        public final int imageY;
+        public final Color color;
+
+        private ShadingPoint(int imageX, int imageY, Color color) {
+            this.imageX = imageX;
+            this.imageY = imageY;
+            this.color = color;
+        }
+    }
+
     /**
      * Renders a scene to a image, using given camera and image resolution. This is a very simple
      * renderer only determining pixel color from the color of the first hit shape. The process of
@@ -25,68 +55,81 @@ public class Raytracer {
      * @return BufferedImage colored after scene contents
      */
     public static BufferedImage renderScene(Scene scene, int resolutionX, int resolutionY) {
-        BufferedImage image = new BufferedImage(resolutionX, resolutionY, BufferedImage.TYPE_INT_ARGB);
+        long testingStart, testingEnd;
+        long shadingStart, shadingEnd;
+        long imageStart, imageEnd;
+
+        testingStart = System.nanoTime();
+        ArrayList<ImageRayHit> points = getPoints(scene, resolutionX, resolutionY);
+        testingEnd = System.nanoTime();
+
+        shadingStart = System.nanoTime();
+        ArrayList<ShadingPoint> shadingPoints = shadePoints(scene, points);
+        shadingEnd = System.nanoTime();
+
+        imageStart = System.nanoTime();
+        BufferedImage image = createImage(resolutionX, resolutionY, shadingPoints);
+        imageEnd = System.nanoTime();
+
+        System.out.format("Total render time %d ms\n", ((shadingEnd - testingStart) / 1000000));
+        System.out.format("Testing for intersections took %d ms\n", (testingEnd - testingStart) / 1000000);
+
+        System.out.format("Shading took %d ms\n", (shadingEnd - shadingStart) / 1000000);
+        System.out.format("Shaded %d points\n", shadingPoints.size());
+
+        System.out.format("Shading took %f ms per 1000nd point\n", ((shadingEnd - shadingStart) / 1000000f) / (shadingPoints.size() / 1000f));
+
+        System.out.format("Creating image took %d ms\n", (imageEnd - imageStart) / 1000000);
+
+        return image;
+    }
+
+    public static ArrayList<ImageRayHit> getPoints(Scene scene, int resolutionX, int resolutionY) {
+        ArrayList<ImageRayHit> hits = new ArrayList<>();
         Iterator<CameraRay> cameraRays = scene.getCamera().raysIterator(resolutionX, resolutionY);
 
         CameraRay cameraRay;
         Ray ray;
-
-        long shadedPoints = 0;
-
-        long shadingTime = 0;
-        long shadingStart, shadingEnd;
-
-        long testingTime = 0;
-        long testStart, testEnd;
-
-        long startTime = System.nanoTime();
-
         while (cameraRays.hasNext()) {
-            testStart = System.nanoTime();
             cameraRay = cameraRays.next();
             ray = cameraRay.ray;
 
             RayHit rayHit = castRay(scene, ray);
 
-            if (rayHit == null) {
-                // No shape hit by ray. Continue to next ray.
-                image.setRGB(cameraRay.x, cameraRay.y, Color.MAGENTA.getRGB());
-                continue;
-            }
+            if (rayHit == null) continue; // Nothing hit continue to next ray
 
-            saurkraut.shapes.Shape shape = rayHit.shape;
+            Shape shape = rayHit.shape;
             Vector3D hit = rayHit.point;
 
-            testEnd = System.nanoTime();
-            testingTime += testEnd - testStart;
-
-
-            shadedPoints++;
-
-            shadingStart = System.nanoTime();
-            Color shapeColor = shape.getColor(hit);
-
-            Color lightColor = PhongShader.shade(scene, shape, hit, ray.direction);
-            //Color lightColor = UnlitShader.shade(shape, hit);
-
-            //System.out.format("x %d, y %d; %s, %s\n", cameraRay.x, cameraRay.y, shapeColor.toString(), lightColor.toString());
-
-            Color finalColor = ColorUtil.multiply(shapeColor, lightColor);
-            shadingEnd = System.nanoTime();
-            shadingTime += shadingEnd - shadingStart;
-
-            image.setRGB(cameraRay.x, cameraRay.y, finalColor.getRGB());
+            hits.add(new ImageRayHit(cameraRay.x, cameraRay.y, ray, rayHit.point, shape));
         }
 
-        long endTime = System.nanoTime();
+        return hits;
+    }
 
-        System.out.format("Total render time %d ms\n", ((endTime - startTime) / 1000000));
-        System.out.format("Testing for intersections took %d ms\n", testingTime / 1000000);
+    public static ArrayList<ShadingPoint> shadePoints(Scene scene, List<ImageRayHit> hits) {
+        ArrayList<ShadingPoint> shadingPoints = new ArrayList<>();
 
-        System.out.format("Shading took %d ms\n", shadingTime / 1000000);
-        System.out.format("Shaded %d points\n", shadedPoints);
+        Color shapeColor;
+        Color lightColor;
+        Color finalColor;
+        for (ImageRayHit hit : hits) {
+            shapeColor = hit.shape.getColor(hit.point);
+            lightColor = PhongShader.shade(scene, hit.shape, hit.point, hit.ray.direction);
+            finalColor = ColorUtil.multiply(shapeColor, lightColor);
 
-        System.out.format("Shading took %f ms per 1000nd point\n", (shadingTime / 1000000f) / (shadedPoints / 1000f));
+            shadingPoints.add(new ShadingPoint(hit.imageX, hit.imageY, finalColor));
+        }
+
+        return shadingPoints;
+    }
+
+    public static BufferedImage createImage(int resolutionX, int resolutionY, List<ShadingPoint> shadingPoints) {
+        BufferedImage image = new BufferedImage(resolutionX, resolutionY, BufferedImage.TYPE_INT_ARGB);
+
+        for (ShadingPoint shadingPoint : shadingPoints) {
+            image.setRGB(shadingPoint.imageX, shadingPoint.imageY, shadingPoint.color.getRGB());
+        }
 
         return image;
     }
